96a08235962455e79c7640d06dcc7e7cf0210a05
Miettine <lauri.miettinen90@gmail.com>
2016-11-25 21:34:40 +0200

    Pondering

--- smart_contracts/fitBlitz.sol
+++ smart_contracts/fitBlitz.sol
@@ -7,21 +7,26 @@ contract FitBlitz {
     
     
     function FitBlitz() {
-        
+        //Can't think of anything that needs to be done as this contract is created.
     } 
     
-    function BeginExercise( address trainee, uint startTime, uint duration,uint goal  ){
-        
-        //
-        //
+    function BeginExercise( address trainee, address charity ,uint startTime, uint duration, uint exerciseGoal  ){
+        //Probably don't need a whole bunch of parameters in the function. 
+		//Could store these to some "exercise"-object.
+		
+        //The details of the bid need to be saved to a collection.
     }
     
-    function CollectReward(address trainee, address charity){
+    function CollectReward(address trainee){
         //Phone would have to poll this function. Not a problem because 
         //the phone knows when the deadline is.
         //If the trainee was succesful, 
         //The contract returns the ether that the bidder sent to the address.
         //Otherwise, the contract sends the money to charity.
+		
+		//This function gets the details of the bid from the collection of bids.
+		//Then it figures out if the bid was succesful, and sends money accordingly.
+		
     }
     
     
