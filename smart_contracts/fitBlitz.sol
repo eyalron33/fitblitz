5f0f39f7547cdce2a9c6e0e38151eb9b82e8a6cc
Miettine <lauri.miettinen90@gmail.com>
2016-11-25 21:24:36 +0200

    The first version!

---
+++ smart_contracts/fitBlitz.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.4.0;
+
+///Created by Lauri Johannes Miettinen in fall 2016.
+
+contract FitBlitz {
+    
+    
+    
+    function FitBlitz() {
+        
+    } 
+    
+    function BeginExercise( address trainee, uint startTime, uint duration,uint goal  ){
+        
+        //
+        //
+    }
+    
+    function CollectReward(address trainee, address charity){
+        //Phone would have to poll this function. Not a problem because 
+        //the phone knows when the deadline is.
+        //If the trainee was succesful, 
+        //The contract returns the ether that the bidder sent to the address.
+        //Otherwise, the contract sends the money to charity.
+    }
+    
+    
+    
+}
