20589e18e356cc0d1c4f0bd48499c2a9ec3e3d5e
eyalron33 <eyalron33@gmail.com>
2016-11-26 12:58:25 +0100

    add pebble js file

---
+++ pebble/js/pebble-js-app.js
@@ -0,0 +1,22 @@
+var imported = document.createElement('script');
+imported.src = 'https://cdn.socket.io/socket.io-1.2.0.js';
+document.head.appendChild(imported);
+
+// Listen for when the watchface is opened
+Pebble.addEventListener('ready', 
+  function(e) {
+    //console.log('PebbleKit JS Shov Hier2.');
+  }
+);
+
+// Listen for when an AppMessage is received
+Pebble.addEventListener('appmessage',
+  function(e) {
+		console.log('sending info to localhost: ' + accelerometerd);
+		pebble_data = JSON.stringify(e.payload);
+
+		var socket = io('http://monsters-neiman.rhcloud.com/');
+    socket.emit('pebble message', pebble_data);
+
+  }                     
+);
