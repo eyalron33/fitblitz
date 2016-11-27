ba1e91ed35cb2d4740f50a3d164d99e3f457372f
eyalron33 <eyalron33@gmail.com>
2016-11-27 10:55:49 +0100

    rigged working-app to blockchain

---
+++ working-app/js/blockchain.js
@@ -0,0 +1,49 @@
+
+	
+
+		// function BeginExercise() {
+		// 	var address = web3.eth.defaultAccount;
+			
+		// 	var res = contracts['FitBlitz'].contract.BeginExercise("0xa9801d7de7c71c7a282ada797e8bfc5192ceaf88", 50, 5);
+		// }
+
+		// function EvaluateExercise() {
+		// 	var res = contracts['FitBlitz'].contract.EvaluateExercise(52, 10);
+		// }
+
+		// // -=- Events -=- //
+		// var ExerciseBegun = contracts['FitBlitz'].contract.ExerciseBegun(function(error, result) {
+  //   if (!error) {
+  //   	document.getElementById('outputs_settings').innerHTML = "Blockchain began exercise"
+  //   	document.getElementById('outputs_lastpage').innerHTML = "Blockchain began exercise"
+  //   	console.log("exercised began");
+		// } else {
+		// 	console.log("failed exercised began");
+		// }});
+
+		// var ExerciseSuccessful = contracts['FitBlitz'].contract.ExerciseSuccessful(function(error, result) {
+  //   if (!error) {
+  //   	document.getElementById('outputs_settings').innerHTML = "Blockchain said that exercise succeded"
+  //   	document.getElementById('outputs_lastpage').innerHTML = "Blockchain said that exercise succeded"
+  //   	console.log("exercise succeed");
+		// } else {
+		// 	console.log("failed exercise succeed");
+		// }});
+
+		// var ExerciseFailed = contracts['FitBlitz'].contract.ExerciseFailed(function(error, result) {
+  //   if (!error) {
+  //   	document.getElementById('outputs_settings').innerHTML = "Blockchain said that exercise failed"
+  //   	document.getElementById('outputs_lastpage').innerHTML = "Blockchain said that exercise failed"
+  //   	console.log("exercise failed");
+		// } else {
+		// 	console.log("failed exercise failed");
+		// }});
+
+		// var ErrorOcurred = contracts['FitBlitz'].contract.ErrorOcurred(function(error, result) {
+  //   if (!error) {
+  //   	document.getElementById('outputs_settings').innerHTML = "Blockchain said that error occured"
+  //   	document.getElementById('outputs_lastpage').innerHTML = "Blockchain said that error occured"
+  //   	console.log("error occured");
+		// } else {
+		// 	console.log("failed error occured");
+		// }});
